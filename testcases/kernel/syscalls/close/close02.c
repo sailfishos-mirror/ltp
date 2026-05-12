a15f42fb6170ae6df004061af0261b86b5e5bfd2
Jinseok Kim <always.starving0@gmail.com>
2026-05-13 00:25:00 +0900

    close02: add test for double close EBADF
    Verify that calling close() on an already closed file descriptor fails
    with EBADF.
    
    This test adds coverage for a common state transition case where a
    previously valid file descriptor becomes invalid after close().
    
    Link: https://lore.kernel.org/ltp/20260512152517.4120-1-always.starving0@gmail.com/
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Jinseok Kim <always.starving0@gmail.com>

--- testcases/kernel/syscalls/close/close02.c
+++ testcases/kernel/syscalls/close/close02.c
@@ -5,17 +5,46 @@
  */
 
 /*\
- * Call close(-1) and expects it to return EBADF.
+ * Verify :manpage:`close(2)` failure cases:
+ *
+ * 1) close(-1) returns EBADF.
+ * 2) closing the same fd twice returns EBADF on the second call.
  */
 
 #include <errno.h>
+#include <fcntl.h>
+
 #include "tst_test.h"
 
-static void run(void)
+static int fd_invalid = -1;
+static int fd_closed = -1;
+
+static struct tcase {
+	const char *desc;
+	int *fd;
+	int exp_errno;
+} tcases[] = {
+	{ "close(-1)", &fd_invalid, EBADF },
+	{ "close same fd twice", &fd_closed, EBADF },
+};
+
+static void verify_close(unsigned int i)
 {
-	TST_EXP_FAIL(close(-1), EBADF);
+	struct tcase *tc = &tcases[i];
+
+	TST_EXP_FAIL(close(*tc->fd), tc->exp_errno, "%s", tc->desc);
+}
+
+static void setup(void)
+{
+	fd_closed = SAFE_OPEN("close02", O_CREAT | O_RDWR, 0600);
+	if (close(fd_closed) == -1)
+		tst_brk(TBROK | TERRNO, "close(%d) failed", fd_closed);
 }
 
 static struct tst_test test = {
-	.test_all = run,
+	.needs_tmpdir = 1,
+	.setup = setup,
+	.tcnt = ARRAY_SIZE(tcases),
+	.test = verify_close,
 };
